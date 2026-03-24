1558b1524c7dad8a716d54ff27a3aefaae0d28a0
Josh Jones <dev@chaoticgoose.uk>
2026-03-24 20:59:53 +0000

    Update jresult

--- twentyfive/src/test/java/uk/chaoticgoose/adventofcode/twentyfive/utils/TestInputLoader.java
+++ twentyfive/src/test/java/uk/chaoticgoose/adventofcode/twentyfive/utils/TestInputLoader.java
@@ -1,18 +1,19 @@
 package uk.chaoticgoose.adventofcode.twentyfive.utils;
 
+import uk.chaoticgoose.jresult.Result;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 import static java.util.Objects.requireNonNull;
-import static uk.chaoticgoose.jresult.ResultUtils.catching;
 
 public class TestInputLoader {
 
     public List<String> load(int day, InputDataModifier modifier) {
         var fileName = "/data/%s_%s.txt".formatted(day, modifier.value());
         Path filePath = Path.of(requireNonNull(getClass().getResource(fileName)).getPath());
-        return catching(() -> Files.readAllLines(filePath)).mapFailure(RuntimeException::new).valueOrThrow();
+        return Result.catching(() -> Files.readAllLines(filePath)).mapFailure(RuntimeException::new).valueOrThrow();
     }
 
     public record InputDataModifier(String value) {
